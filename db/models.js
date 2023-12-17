fd7bb7c799fa578b00c4ac8edf2c73f9f2d8e36c
Finley A.M.O. <finleyyowen@gmail.com>
2023-12-17 15:05:48 +1100

    First production commit

--- db/models.js
+++ db/models.js
@@ -85,21 +85,20 @@ Entry.belongsToMany(Creator, { through: "EntryCreator" });
 // Hashtag.belongsToMany(Entry, { through: "EntryHashtag"})
 
 /* Uncomment to initialise empty database and add the first creator and entry */
-
 // (async () => {
 //     await sequelize.sync();
 //     await Creator.create({
-//       username: 'finleyamo',
-//       displayname: 'Finley Alva M.O.',
-//       descriptors: 'they/them',
-//       bio: 'Just your average human.',
-//       email: 'notmyactualemail@mxture.space',
-//       pfpname: 'finleyamo-pfp.jpeg',
+//       username: 'chaispice',
+//       displayname: 'Chai Spice Latte',
+//       descriptors: 'she/they',
+//       bio: 'Not just your average greyhound.',
+//       email: 'notanactualemail@chai.spice',
+//       pfpname: 'chaispice-pfp.png',
 //       Entries: [{
 //         headline: 'Welcome to MXTURE!',
 //         byline: 'We are really excited to have you here!',
 //         path: 'welcome-to-mxture',
-//         coverimname: 'glitter.png',
-//         content: '<p>This is the first entry to ever be published to the journal!</p><p>The images are generated using OpenAI\'s DALL-E model via Canva.</p>'
+//         coverimname: 'glitter-1.png',
+//         content: '<p>This is the first entry to ever be published to the journal!</p><p>The cover images are generated using OpenAI\'s DALL-E model via Canva.</p>'
 //       }]}, { include: [{ model: Entry, as: 'Entries' }]})
 // })();
